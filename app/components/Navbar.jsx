ec7d8f79d94526516bfa7f481b569a2b4a08fb72
nayem567 <inew2732@gmail.com>
2026-01-14 18:58:35 +0600

    Initial template setup

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import Logo from "@/app/components/Logo";
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <nav className="py-4 md:py-6 border-b">
+      <div className="container mx-auto flex items-center justify-between gap-x-6">
+        <Logo />
+      </div>
+    </nav>
+  );
+}
